259ae2c7b9140a41b323be061e1a092e58a5643e
LaVasik11 <kankiyag@yandex.ru>
2023-09-22 20:53:13 +0400

    добавил авторизацию на сайт, теперь только автаризованые люди могут создавать и удалять посты.

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from django.conf import settings
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('news/', include('news.urls')),
+    path('users/', include('users.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
